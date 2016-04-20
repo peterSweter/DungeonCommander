153fd9bb1d1cd191ff607681c94185652cbe73ad
Piotr <peteruchwat@gmail.com[D>
2016-04-20 23:32:19 +0200

    init repo

---
+++ desktop/src/com/sweter/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.sweter.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sweter.game.dungeonCommander;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new dungeonCommander(), config);
+	}
+}
