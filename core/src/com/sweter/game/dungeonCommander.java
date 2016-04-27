1114d15970ed0c22a388e2a013f2dea6f79dfcfb
Piotr <peteruchwat@gmail.com[D>
2016-04-27 23:09:50 +0200

    folder screen

--- core/src/com/sweter/game/dungeonCommander.java
+++ core/src/com/sweter/game/dungeonCommander.java
@@ -1,12 +1,13 @@
 package com.sweter.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class dungeonCommander extends ApplicationAdapter {
+public class dungeonCommander extends Game {
 	SpriteBatch batch;
 	Texture img;
 	
@@ -23,5 +24,10 @@ public class dungeonCommander extends ApplicationAdapter {
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
+	}
+
+	@Override
+	public void dispose(){
+		super.dispose();
 	}
 }
